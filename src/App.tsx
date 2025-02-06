6ae5fdc76aa8c1e232f9ed6005ad4ea70dd1fa6b
Xian Meg Calma <xianmegcalma23@gmail.com>
2025-02-07 00:27:19 +0800

    initial-commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <h1 className="name">Kodai Kato</h1>
+      <div className="box">
+        <p>CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
